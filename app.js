e064ba98541d082275a81709c549b55d46e9710b
CMP - Isaac Trost <CMP_IsTrost@ects.org>
2024-12-09 14:40:42 -0500

    Redid everything.

--- app.js
+++ app.js
@@ -1,82 +1,71 @@
-/* Board */
-const board = [
-  ["P5", "P2", "P3", "Y2", "W1", "B1"],
-  ["R6", "R4", "Y3", "W2", "G2", "R2"],
-  ["B4", "G5", "G3", "P4", "G6", "W4"],
-  ["W6", "B6", "B2", "P6", "Y5", "P1"],
-  ["R3", "Y4", "W3", "R1", "B5", "Y6"],
-  ["B3", "G4", "W5", "G1", "R5", "Y1"],
-];
+class Board
+{
+  constructor(){
+    this._board = [
+      ["P5", "P2", "P3", "Y2", "W1", "B1"],
+      ["R6", "R4", "Y3", "W2", "G2", "R2"],
+      ["B4", "G5", "G3", "P4", "G6", "W4"],
+      ["W6", "B6", "B2", "P6", "Y5", "P1"],
+      ["R3", "Y4", "W3", "R1", "B5", "Y6"],
+      ["B3", "G4", "W5", "G1", "R5", "Y1"],
+    ];
+  }
 
-let tempBoard = [];
+  /* Get a random integer from 0 to n */
+  static #getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
-function splitBoard() {
-  const matricies = [];
-  let matrix1 = [
-    [tempBoard[0][0], tempBoard[0][1], tempBoard[0][2]],
-    [tempBoard[1][0], tempBoard[1][1], tempBoard[1][2]],
-    [tempBoard[2][0], tempBoard[2][1], tempBoard[2][2]],
-  ];
+  static #extractBoardMatrix(rowStart, rowEnd, colStart, colEnd){
+    let extractedMatrix = []
+    for(row=rowStart;row<rowEnd;row++){
+      let row = []
+      for(col=colStart;col<colEnd;col++){
+        row.push(this._board[row][col])
+      }
+      extractedMatrix.push(row)
+    }
+    return extractedMatrix
+  }
 
-  let matrix2 = [
-    [tempBoard[0][3], tempBoard[0][4], tempBoard[0][5]],
-    [tempBoard[1][3], tempBoard[1][4], tempBoard[1][5]],
-    [tempBoard[2][3], tempBoard[2][4], tempBoard[2][5]],
-  ];
+  static #splitBoard(){
+    return [this.#extractBoardMatrix(0,3,0,3), this.#extractBoardMatrix(0,3,3,6), this.#extractBoardMatrix(3,6,0,3), this.#extractBoardMatrix(3,6,3,6)]
+  }
 
-  let matrix3 = [
-    [tempBoard[3][0], tempBoard[3][1], tempBoard[3][2]],
-    [tempBoard[4][0], tempBoard[4][1], tempBoard[4][2]],
-    [tempBoard[5][0], tempBoard[5][1], tempBoard[5][2]],
-  ];
+  /* Rotate and return a matrix from the board. */
+  /* The index represents the quadrant of the board in order from top left to bottom right. */
+  static #rotateMatrix(index, clockwise) {
+    let sections = splitBoard(tempBoard);
+    let matrix = sections[index];
+    /* Temporary matrix for rotation. */
+    let tempMatrix = []
+    if (clockwise) {
+      for(row=2;row>=0;row--){
+        for(col=0;col<3;col++){
+          tempMatrix.push(matrix[row][col])
+        }
+      }
+    } else {
+      for(row=0;row>=3;row--){
+        for(col=2;col>=0;col--){
+          tempMatrix.push(matrix[row][col])
+        }
+      }
+    }
+    return tempMatrix;
+  }
 
-  let matrix4 = [
-    [tempBoard[3][3], tempBoard[3][4], tempBoard[3][5]],
-    [tempBoard[4][3], tempBoard[4][4], tempBoard[4][5]],
-    [tempBoard[5][3], tempBoard[5][4], tempBoard[5][5]],
-  ];
-
-  matricies.push(matrix1);
-  matricies.push(matrix2);
-  matricies.push(matrix3);
-  matricies.push(matrix4);
-  return matricies;
 }
 
-/* Color dictionary used to convert letters to the corresponding colors */
-const colorDict = {
-  P: "pink",
-  Y: "yellow",
-  W: "white",
-  B: "blue",
-  R: "red",
-  G: "green",
-};
+/* Board */
+
+
+let tempBoard = [];
 
 /* The current path */
 let cellPath = [];
 
-function rotateMatrix(index, clockwise) {
-  let matricies = splitBoard(tempBoard);
-  let matrix = matricies[index];
-  if (clockwise) {
-    matrix = [
-      [matrix[2][0], matrix[1][0], matrix[0][0]],
-      [matrix[2][1], matrix[1][1], matrix[0][1]],
-      [matrix[2][2], matrix[1][2], matrix[0][2]],
-    ];
-  } else {
-    matrix = [
-      [matrix[0][2], matrix[1][2], matrix[2][2]],
-      [matrix[0][1], matrix[1][1], matrix[2][1]],
-      [matrix[0][0], matrix[1][0], matrix[2][0]],
-    ];
-  }
 
-  matricies[index] = matrix;
-  buildBoard(matricies);
-  generateBoard();
-}
 
 /* Construct the board from the matricies. */
 function buildBoard(matricies) {
@@ -91,98 +80,22 @@ function buildBoard(matricies) {
   console.log(tempBoard);
 }
 
-/* Extract the color and number from a cell string */
-function parseCellString(string) {
-  return [
-    colorDict[string.substring(0, 1)],
-    string.substring(1, 2),
-    string.substring(2, 3),
-  ];
-}
-
-/* Check the validity of a movement between two cells */
-function checkMoveValidity(row1, col1, row2, col2) {
-  let cell1 = tempBoard[row1][col1];
-  let color1 = colorDict[cell1.substring(0, 1)];
-  let number1 = cell1.substring(1, 2);
-  console.log(`CELL1: ${cell1}, ROW: ${row1}, COL: ${col1}`);
-
-  let cell2 = tempBoard[row2][col2];
-  let color2 = colorDict[cell2.substring(0, 1)];
-  let number2 = cell2.substring(1, 2);
-  console.log(`CELL2: ${cell2}, ROW: ${row2}, COL: ${col2}`);
-
-  if (
-    (color1 == color2 || number1 == number2) &&
-    (row1 == row2 || col1 == col2)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-/* Check for win */
-function checkWin(row, col) {
-  if (tempBoard[row][col].includes("E")) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function reportWin(moves) {
-  console.log(`You won in ${moves} move(s)!`);
-  openLightbox(moves);
-  cellPath = [];
-}
-
-/* Function used as an event for cell clicks. */
-function clickCell() {
-  let row = parseInt(this.getAttribute("row"));
-  let col = parseInt(this.getAttribute("col"));
-  if (
-    checkMoveValidity(
-      cellPath[cellPath.length - 1][0],
-      cellPath[cellPath.length - 1][1],
-      row,
-      col
-    )
-  ) {
-    console.log("Valid move.");
-    cellPath.push([row, col]);
-    if (checkWin(row, col)) {
-      reportWin(cellPath.length - 1);
-    }
-  } else {
-    console.log("Invalid move.");
-  }
-}
-
 /* Place the start and end cells on the board */
 function placeStartEnd() {
   /* Define start and end rows */
-  let startRow = getRandomInt(6);
-  let startCol = getRandomInt(6);
-  let endRow = getRandomInt(6);
-  let endCol = getRandomInt(6);
+  let [startRow, startCol] = [getRandomInt(6), getRandomInt(6)];
+  let [endRow, endCol] = [getRandomInt(6), getRandomInt(6)]
 
   /* Add start pos to cellPath */
   cellPath.push([startRow, startCol]);
 
   /* Make sure the end cell position isnt within 1 cell of the start cell. */
   while (
-    (startRow === endRow ||
-      startRow + 1 === endRow ||
-      startRow - 1 === endRow) &&
-    (startCol === endCol || startCol + 1 === endCol || startCol - 1 === endCol)
+    (Math.abs(startRow - endRow) <= 1 && Math.abs(startCol - endCol) <= 1)
   ) {
-    endRow = getRandomInt(6);
-    endCol = getRandomInt(6);
+    [endRow, endCol] = [getRandomInt(6), getRandomInt(6)]
   }
-
-  tempBoard[startRow][startCol] += "S";
-  tempBoard[endRow][endCol] += "E";
+  /* Todo: actually place the start and end spots. */
 }
 
 /* Generate the board */
@@ -230,26 +143,10 @@ function generateBoard() {
   }
 }
 
-/* Get a random integer from 0 to n */
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-/* Locate a cell div on the board with its row and column */
-function locateElement(row, col) {
-  return document.querySelector(`[row="${row}"][col="${col}"]`);
-}
-
-buildBoard();
-placeStartEnd();
-generateBoard();
-
 function closeLightbox() {
   const lightbox = document.getElementsByClassName("lightbox")[0];
   lightbox.classList.toggle("hidden");
-  buildBoard();
-  placeStartEnd();
-  generateBoard();
+  /* Rebuild board here when that function is available. */
 }
 
 function openLightbox(moves) {
