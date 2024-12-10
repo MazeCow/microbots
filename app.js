ef51d093dae533d80278a9f4bee2126ed6e9f106
CMP - Isaac Trost <CMP_IsTrost@ects.org>
2024-12-10 14:42:23 -0500

    balls

--- app.js
+++ app.js
@@ -1,147 +1,210 @@
-class Board
-{
-  constructor(){
+class Cell {
+  constructor(color, number) {
+    this.color = color;
+    this.number = number;
+    this.isStart = false;
+    this.isEnd = false;
+    this.occupied = false;
+  }
+}
+
+class Board {
+  constructor() {
     this._board = [
-      ["P5", "P2", "P3", "Y2", "W1", "B1"],
-      ["R6", "R4", "Y3", "W2", "G2", "R2"],
-      ["B4", "G5", "G3", "P4", "G6", "W4"],
-      ["W6", "B6", "B2", "P6", "Y5", "P1"],
-      ["R3", "Y4", "W3", "R1", "B5", "Y6"],
-      ["B3", "G4", "W5", "G1", "R5", "Y1"],
+      [
+        new Cell("pink", 5),
+        new Cell("pink", 2),
+        new Cell("pink", 3),
+        new Cell("yellow", 2),
+        new Cell("white", 1),
+        new Cell("blue", 1),
+      ],
+      [
+        new Cell("red", 6),
+        new Cell("red", 4),
+        new Cell("yellow", 3),
+        new Cell("white", 2),
+        new Cell("green", 2),
+        new Cell("red", 2),
+      ],
+      [
+        new Cell("blue", 4),
+        new Cell("green", 5),
+        new Cell("green", 3),
+        new Cell("pink", 4),
+        new Cell("green", 6),
+        new Cell("white", 4),
+      ],
+      [
+        new Cell("white", 6),
+        new Cell("blue", 6),
+        new Cell("blue", 2),
+        new Cell("pink", 6),
+        new Cell("yellow", 5),
+        new Cell("pink", 1),
+      ],
+      [
+        new Cell("red", 3),
+        new Cell("yellow", 4),
+        new Cell("white", 3),
+        new Cell("red", 1),
+        new Cell("blue", 5),
+        new Cell("yellow", 6),
+      ],
+      [
+        new Cell("blue", 3),
+        new Cell("green", 4),
+        new Cell("white", 5),
+        new Cell("green", 1),
+        new Cell("red", 5),
+        new Cell("yellow", 1),
+      ],
     ];
   }
 
   /* Get a random integer from 0 to n */
-  static #getRandomInt(max) {
+  getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-  static #extractBoardMatrix(rowStart, rowEnd, colStart, colEnd){
-    let extractedMatrix = []
-    for(row=rowStart;row<rowEnd;row++){
-      let row = []
-      for(col=colStart;col<colEnd;col++){
-        row.push(this._board[row][col])
+  extractBoardMatrix(rowStart, rowEnd, colStart, colEnd) {
+    let extractedMatrix = [];
+    for (let row = rowStart; row < rowEnd; row++) {
+      let rows = [];
+      for (let col = colStart; col < colEnd; col++) {
+        try {
+          rows.push(this._board[row][col]);
+        } catch (error) {
+          alert(JSON.stringify(this._board));
+
+          console.error(error);
+        }
       }
-      extractedMatrix.push(row)
+      extractedMatrix.push(rows);
     }
-    return extractedMatrix
+    return extractedMatrix;
   }
 
-  static #splitBoard(){
-    return [this.#extractBoardMatrix(0,3,0,3), this.#extractBoardMatrix(0,3,3,6), this.#extractBoardMatrix(3,6,0,3), this.#extractBoardMatrix(3,6,3,6)]
+  splitBoard() {
+    const arr = [
+      this.extractBoardMatrix(0, 3, 0, 3),
+      this.extractBoardMatrix(0, 3, 3, 6),
+      this.extractBoardMatrix(3, 6, 0, 3),
+      this.extractBoardMatrix(3, 6, 3, 6),
+    ];
+    alert(JSON.stringify(arr));
+    return [
+      this.extractBoardMatrix(0, 3, 0, 3),
+      this.extractBoardMatrix(0, 3, 3, 6),
+      this.extractBoardMatrix(3, 6, 0, 3),
+      this.extractBoardMatrix(3, 6, 3, 6),
+    ];
   }
 
   /* Rotate and return a matrix from the board. */
   /* The index represents the quadrant of the board in order from top left to bottom right. */
-  static #rotateMatrix(index, clockwise) {
-    let sections = splitBoard(tempBoard);
+  rotateMatrix(index, clockwise) {
+    let sections = this.splitBoard(this._board);
     let matrix = sections[index];
     /* Temporary matrix for rotation. */
-    let tempMatrix = []
+    let tempMatrix = [];
     if (clockwise) {
-      for(row=2;row>=0;row--){
-        for(col=0;col<3;col++){
-          tempMatrix.push(matrix[row][col])
+      for (let row = 2; row >= 0; row--) {
+        let rowArray = [];
+        for (let col = 0; col < 3; col++) {
+          rowArray.push(matrix[row][col]);
         }
+        tempMatrix.push(rowArray);
       }
     } else {
-      for(row=0;row>=3;row--){
-        for(col=2;col>=0;col--){
-          tempMatrix.push(matrix[row][col])
+      for (let row = 0; row < 3; row++) {
+        let rowArray = [];
+        for (let col = 2; col >= 0; col--) {
+          rowArray.push(matrix[row][col]);
         }
+        tempMatrix.push(rowArray);
       }
     }
-    return tempMatrix;
+    sections[index] = tempMatrix;
+    this._board = this.buildBoard(sections);
   }
 
+  buildBoard(matricies) {
+    matricies = this.splitBoard();
+    let matrixRows = [];
+    for (let i = 0; i < matricies.length / 2; i++) {
+      let matrixRow = matricies[i].map((row, j) => {
+        [...row, ...matricies[j]];
+      });
+      matrixRows.push(matrixRow);
+    }
+    const b = [...matrixRows[0], ...matrixRows[1]];
+    return b;
+  }
+
+  static placeStartEndCells() {
+    /* Define start and end rows */
+    let [startRow, startCol] = [this.getRandomInt(6), this.getRandomInt(6)];
+    let [endRow, endCol] = [this.getRandomInt(6), this.getRandomInt(6)];
+
+    /* Add start pos to cellPath */
+    cellPath.push([startRow, startCol]);
+
+    /* Make sure the end cell position isnt within 1 cell of the start cell. */
+    while (
+      Math.abs(startRow - endRow) <= 1 &&
+      Math.abs(startCol - endCol) <= 1
+    ) {
+      [endRow, endCol] = [this.getRandomInt(6), this.getRandomInt(6)];
+    }
+    /* Todo: actually place the start and end spots. */
+    this._board[startRow][startCol].isStart = true;
+    this._board[endRow][endCol].isStart = true;
+  }
+
+  generateBoard(gridDiv) {
+    /* Select board grid element on webpage */
+    gridDiv.classList.add("grid");
+
+    /* Clear the board */
+    grid.innerHTML = "";
+
+    /* Iterate through the board and create cells on the webpage. */
+    for (let row = 0; row < this._board.length; row++) {
+      for (let col = 0; col < this._board[row].length; col++) {
+        /* Parse the data from the board cell. */
+        let cell = this._board[row][col];
+
+        /* Create a new cell and set it's attributes */
+        let divCell = document.createElement("div");
+        divCell.setAttribute("row", row);
+        divCell.setAttribute("col", col);
+        divCell.classList.add(cell.color, "cell");
+
+        /* Create a number container and append it to the cell as a child. */
+        let textContainer = document.createElement("div");
+        textContainer.classList.add("number-container");
+        textContainer.innerHTML = cell.number;
+        divCell.appendChild(textContainer);
+
+        /* Add an event listener to the cell. */
+        divCell.addEventListener("click", clickCell);
+
+        /* Append the cell to the grid */
+        grid.append(divCell);
+      }
+    }
+  }
 }
-
-/* Board */
-
-
-let tempBoard = [];
 
 /* The current path */
 let cellPath = [];
 
-
-
-/* Construct the board from the matricies. */
-function buildBoard(matricies) {
-  tempBoard = structuredClone(board);
-  if (matricies == undefined) {
-    matricies = splitBoard(tempBoard);
-  }
-  for (let i = 0; i < 3; i++) {
-    tempBoard[i] = matricies[0][i].concat(matricies[1][i]);
-    tempBoard[i + 3] = matricies[2][i].concat(matricies[3][i]);
-  }
-  console.log(tempBoard);
-}
-
-/* Place the start and end cells on the board */
-function placeStartEnd() {
-  /* Define start and end rows */
-  let [startRow, startCol] = [getRandomInt(6), getRandomInt(6)];
-  let [endRow, endCol] = [getRandomInt(6), getRandomInt(6)]
-
-  /* Add start pos to cellPath */
-  cellPath.push([startRow, startCol]);
-
-  /* Make sure the end cell position isnt within 1 cell of the start cell. */
-  while (
-    (Math.abs(startRow - endRow) <= 1 && Math.abs(startCol - endCol) <= 1)
-  ) {
-    [endRow, endCol] = [getRandomInt(6), getRandomInt(6)]
-  }
-  /* Todo: actually place the start and end spots. */
+function clickCell() {
+  return;
 }
 
 /* Generate the board */
-function generateBoard() {
-  /* Select board grid element on webpage */
-  const grid = document.getElementsByClassName("grid")[0];
-
-  /* Clear the board */
-  grid.innerHTML = "";
-
-  /* Iterate through the board and create cells on the webpage. */
-  for (let row = 0; row < tempBoard.length; row++) {
-    for (let col = 0; col < tempBoard[row].length; col++) {
-      /* Parse the data from the board cell. */
-      let cellData = parseCellString(tempBoard[row][col]);
-      let color = cellData[0];
-      let number = cellData[1];
-      let property = cellData[2];
-
-      /* Create a new cell and set it's attributes */
-      let divCell = document.createElement("div");
-      divCell.setAttribute("row", row);
-      divCell.setAttribute("col", col);
-      divCell.classList.add(color, "cell");
-
-      /* Add a class to the cell based on it's property. */
-      if (property == "S") {
-        divCell.classList.add("start");
-      } else if (property == "E") {
-        divCell.classList.add("end");
-      }
-
-      /* Create a number container and append it to the cell as a child. */
-      let textContainer = document.createElement("div");
-      textContainer.classList.add("number-container");
-      textContainer.innerHTML = number;
-      divCell.appendChild(textContainer);
-
-      /* Add an event listener to the cell. */
-      divCell.addEventListener("click", clickCell);
-
-      /* Append the cell to the grid */
-      grid.append(divCell);
-    }
-  }
-}
 
 function closeLightbox() {
   const lightbox = document.getElementsByClassName("lightbox")[0];
@@ -155,3 +218,8 @@ function openLightbox(moves) {
   movesText.innerHTML = moves;
   lightbox.classList.toggle("hidden");
 }
+
+const grid = document.getElementsByClassName("grid")[0];
+
+board = new Board();
+board.generateBoard(grid);
