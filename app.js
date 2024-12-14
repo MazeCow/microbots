78f583af6ba29b26da29e78f3b8f44a14c184d8e
Isaac Trost <isaacmichaeltrost@gmail.com>
2024-12-14 00:26:56 -0500

    poop

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ class BoardTools {
     return grid.children[row * 6 + col];
   }
 
+  static getCellCoords(cell) {
+    const grid = document.getElementsByClassName("grid")[0];
+    const index = grid.children.findIndex(cell);
+    return [Math.round((index + 1) / 6), (index + 1) % 6];
+  }
+
   /* Get a random integer from 0 to max. */
   static randInt(max) {
     return Math.floor(Math.random() * max);
@@ -26,7 +32,8 @@ class BoardTools {
 }
 
 class Board {
-  constructor() {
+  constructor(gameLogicHandler) {
+    this.gameLogicHandler = gameLogicHandler;
     this._board = [
       [
         new Cell(4, 5),
@@ -107,36 +114,36 @@ class Board {
   /* Rotate and return a matrix from the board. */
   rotate(index, clockwise) {
     let sections = this.divide();
-    let matrix = sections[index];
+    let quadrant = sections[index];
 
     /* Temporary matrix for rotation. */
-    let tempMatrix = [];
+    let tempQuadrant = [];
     if (clockwise) {
       for (let col = 0; col < 3; col++) {
         let rowArray = [];
         for (let row = 2; row >= 0; row--) {
-          rowArray.push(matrix[row][col]);
+          rowArray.push(quadrant[row][col]);
         }
-        tempMatrix.push(rowArray);
+        tempQuadrant.push(rowArray);
       }
     } else {
       for (let col = 2; col >= 0; col--) {
         let rowArray = [];
         for (let row = 0; row < 3; row++) {
-          rowArray.push(matrix[row][col]);
+          rowArray.push(quadrant[row][col]);
         }
-        tempMatrix.push(rowArray);
+        tempQuadrant.push(rowArray);
       }
     }
-    sections[index] = tempMatrix;
+    sections[index] = tempQuadrant;
     this.recombine(sections);
   }
 
   /* Builds the board from a set of four matricies. */
-  recombine(matricies) {
+  recombine(quadrants) {
     /* Get the quadrants of the board if they were not provided. */
-    if (matricies == undefined) {
-      matricies = this.divide();
+    if (quadrants == undefined) {
+      quadrants = this.divide();
     }
 
     /* Recombine all the matricies . */
@@ -144,7 +151,7 @@ class Board {
     for (let j = 0; j < 3; j += 2) {
       let tempRows = [];
       for (let i = 0; i < 3; i++) {
-        tempRows.push([...matricies[j][i], ...matricies[j + 1][i]]);
+        tempRows.push([...quadrants[j][i], ...quadrants[j + 1][i]]);
       }
       matrixRows.push(...tempRows);
     }
@@ -183,44 +190,132 @@ class Board {
       }
     }
   }
+
+  /* Rotate and update a quadrant. */
+  rotateAndUpdate(index, clockwise) {
+    this.rotate(index, clockwise);
+    this.place();
+  }
 }
 
-class MovementHandler {
+class GameLogicHandler {
   constructor() {
-    this.startPos = this.randomStartPos();
-    this.endPos = this.randomEndPos();
-    this.movementPath = [this.startPos];
+    this.ongoing = false;
+    this.board = new Board(this);
+    this.movementHandler = new MovementHandler(this);
   }
 
-  /* Returns the move count. */
-  get moveCount() {
-    return this.moveList.length;
+  startGame() {
+    this.ongoing = true;
+    this.addClickListeners();
   }
 
-  /* Returns the currently occupied cell. */
-  get occupiedCell() {
-    return this.board.occupiedCell;
+  endGame() {
+    this.ongoing = false;
   }
 
-  randomStartPos() {
-    return [BoardTools.getRandomInt(6), BoardTools.getRandomInt(6)];
+  rotate(index, clockwise) {
+    if (this.ongoing) {
+      console.log("You can't rotate a quadrant when the game is ongoing!");
+    } else {
+      this.board.rotateAndUpdate(index, clockwise);
+    }
   }
-  randomEndPos() {
-    let [endRow, endCol] = [
-      BoardTools.getRandomInt(6),
-      BoardTools.getRandomInt(6),
-    ];
-    /* Make sure the end cell position isnt within 1 cell of the start cell. */
-    while (
-      Math.abs(startPos[0] - endRow) <= 1 &&
-      Math.abs(startPos[1] - endCol) <= 1
-    ) {
-      [endRow, endCol] = [
-        BoardTools.getRandomInt(6),
-        BoardTools.getRandomInt(6),
-      ];
+
+  addClickListeners() {
+    for (let row = 0; row < this._board.length; row++) {
+      for (let col = 0; col < this._board.length; col++) {
+        const cell = BoardTools.findCellDiv(row, col);
+        cell.addEventListener("click");
+      }
     }
   }
 }
 
-const board = new Board();
+class MovementHandler {
+  constructor(gameLogicHandler) {
+    this.startPos = this.randomStartPos();
+    this.endPos = this.randomEndPos();
+    this._path = [this.startPos];
+    this.occupiedCell = this.startPos;
+    this.gameLogicHandler = gameLogicHandler;
+  }
+
+  get board() {
+    return this.gameLogicHandler.board._board;
+  }
+
+  /* Returns the move count. */
+  get moveCount() {
+    return this._path.length;
+  }
+
+  randomStartPos() {
+    return [BoardTools.randInt(6), BoardTools.randInt(6)];
+  }
+
+  randomEndPos() {
+    let [endRow, endCol] = [BoardTools.randInt(6), BoardTools.randInt(6)];
+    /* Make sure the end cell position isnt within 1 cell of the start cell. */
+    while (
+      Math.abs(this.startPos[0] - endRow) <= 1 &&
+      Math.abs(this.startPos[1] - endCol) <= 1
+    ) {
+      [endRow, endCol] = [BoardTools.randInt(6), BoardTools.randInt(6)];
+    }
+  }
+
+  validMove(startPos, endPos) {
+    const startCell = board[startPos[0]][startPos[1]];
+    const destinationCell = board[endPos[0]][endPos[1]];
+
+    /* Checks if the start and end destinations are in the same row or column. */
+    if (startPos[0] != endPos[0] && startPos[1] != endPos[1]) {
+      return false;
+    }
+
+    /* Checks if the start and end cell have the same color or number. */
+    if (
+      startCell.color != destinationCell.color &&
+      startCell.number != destinationCell.number
+    ) {
+      return false;
+    }
+
+    /* The move is valid if it passes the other checks. */
+    return true;
+  }
+
+  move(destinationPos) {
+    /* Remove occupied class from the previous cell. */
+    const previousCell = BoardTools.findCellDiv(
+      this.occupiedCell[0],
+      this.occupiedCell[1]
+    );
+    previousCell.classList.remove("occupied");
+
+    /* Added occupied class to the new cell. */
+    const newCell = BoardTools.findCellDiv(
+      this.destinationCell[0],
+      this.destinationCell[1]
+    );
+    newCell.classList.add("occupied");
+
+    /* Update path and current occupied cell. */
+    this._path.add(coords);
+    this.occupiedCell = coords;
+  }
+
+  clickCell(event) {
+    /* Gets the position of the clicked cell. */
+    const destinationPos = BoardTools.getCellCoords(event.target);
+    const isValidMove = this.validMove(this.occupiedCell, endPos);
+    if (isValidMove) {
+      this.move(destinationPos);
+    } else {
+      console.log("Invalid cell move.");
+    }
+  }
+}
+
+const g = new GameLogicHandler();
